c9af9b950c183247a8467444e190fe92f163d82e
gato365 <immanuelwilliams@gato365.com>
2023-01-10 08:31:36 -0800

    Fixed node mon and puting functionality

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,6 +24,22 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
+});
+
+
+
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
@@ -48,43 +64,36 @@ router.get('/:id', async (req, res) => {
 
 });
 
-router.post('/', async (req, res) => {
-  // create a new category
-  try {
-    const categoryData = await Category.create(req.body);
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
 
-});
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 
-  try {
-    const categoryData = await Category.findById(req.params.id, 
-      (err,category) => {
-      // Update the category with the request body
-
-      category.category_name = req.body.category_name; 
-
-
-      // Save the post
-      category.save((err, updatedcategory) => {
-        if (err) {
-          res.status(500).send(err);
-        }
-        res.status(200).send(updatedcategory);
-      });
+  //Calls the update method on the Book model
+  await Category.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      category_name: req.body.category_name,
+    },
+    {
+      // Gets a book based on the book_id given in the request parameters
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      res.json(updatedCategory);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
     });
-
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
-
 });
+
+
+
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
